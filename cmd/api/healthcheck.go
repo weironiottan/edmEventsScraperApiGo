e623d8cdc21f3ffdf97ecea0a2cd6cd6ebbc825e
Christian Gabrielsson <chrisgabrielsson@protonmail.com>
2023-07-29 19:04:47 -0700

    dummy pr

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,4 +11,5 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
+
 }
