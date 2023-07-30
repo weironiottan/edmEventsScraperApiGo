9500f622ea010ab1e82d9ddddd4b918e16af1dd1
Christian Gabrielsson <65839186+weironiottan@users.noreply.github.com>
2023-07-29 20:48:40 -0700

    Add mongo db (#7)
    * added mongoDB connection file
    
    * added mongo DB support to delete the collection and add new events
    
    ---------
    
    Co-authored-by: Christian Gabrielsson <chrisgabrielsson@protonmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,30 +30,23 @@ type config struct {
 // and middleware. At the moment this only contains a copy of the config struct and a
 // logger, but it will grow to include a lot more as our build progresses.
 type application struct {
-	config config
-	logger *log.Logger
+	config   config
+	logger   *log.Logger
+	dbConfig dbConfiguration
+}
+
+type dbConfiguration struct {
+	mongoUrl      string
+	mongoHost     string
+	mongoUser     string
+	mongoPassword string
+	mongoPort     string
 }
 
 func main() {
 	// Declare an instance of the config struct.
 	var cfg config
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-	portStr := os.Getenv("PORT")
-
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		log.Fatalf("Error converting port to integer: %v", err)
-	}
-
-	// Read the value of the port and env command-line flags into the config struct. We
-	// default to using the port number 4000 and the environment "development" if no
-	// corresponding flags are provided.
-	flag.IntVar(&cfg.port, "port", port, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.Parse()
+	var dbConfig dbConfiguration
 
 	// Initialize a new logger which writes messages to the standard out stream,
 	// prefixed with the current date and time.
@@ -62,9 +55,18 @@ func main() {
 	// Declare an instance of the application struct, containing the config struct and
 	// the logger.
 	app := &application{
-		config: cfg,
-		logger: logger,
+		config:   cfg,
+		logger:   logger,
+		dbConfig: dbConfig,
 	}
+
+	// Read the value of the port and env command-line flags into the config struct. We
+	// default to using the port number 4000 and the environment "development" if no
+	// corresponding flags are provided.
+	app.fetchEnvVariables()
+	flag.IntVar(&cfg.port, "port", app.config.port, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
 
 	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
 	// to the healthcheckHandler method (which we will create in a moment).
@@ -73,6 +75,7 @@ func main() {
 	mux.HandleFunc("/v1/fetchWynnEdmEvents", app.fetchWynnEdmEvents)
 	mux.HandleFunc("/v1/fetchHakkasanGroupEdmEvents", app.fetchHakkasanGroupEdmEvents)
 	mux.HandleFunc("/v1/fetchZoukEdmEvents", app.fetchZoukEdmEvents)
+	mux.HandleFunc("/v1/addEdmEventsToLasVegasEdmEventsCollection", app.addEdmEventsToLasVegasEdmEventsCollection)
 
 	// Declare a HTTP server with some sensible timeout settings, which listens on the
 	// port provided in the config struct and uses the servemux we created above as the
@@ -87,6 +90,28 @@ func main() {
 
 	// Start the HTTP server.
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err = srv.ListenAndServe()
+	err := srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func (app *application) fetchEnvVariables() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	portStr := os.Getenv("PORT")
+
+	port, err := strconv.Atoi(portStr)
+	app.config.port = port
+	if err != nil {
+		log.Fatalf("Error converting port to integer: %v", err)
+	}
+
+	app.dbConfig = dbConfiguration{
+		mongoUrl:      os.Getenv("MONGO_URL"),
+		mongoHost:     os.Getenv("MONGOHOST"),
+		mongoPort:     os.Getenv("MONGOPORT"),
+		mongoUser:     os.Getenv("MONGOUSER"),
+		mongoPassword: os.Getenv("MONGOPASSWORD"),
+	}
 }
