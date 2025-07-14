1597c397940a0767ed2987f1266ed31f64da42fa
Christian Gabrielsson <65839186+weironiottan@users.noreply.github.com>
2025-07-13 19:50:17 -0700

    added support for scraping with pagination for liv (#21)
    Co-authored-by: Christian Gabrielsson <chrisgabrielsson@protonmail.com>

--- cmd/fetchLivEdmEvents.go
+++ cmd/fetchLivEdmEvents.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gocolly/colly"
+	"github.com/PuerkitoBio/goquery"
 	"net/http"
 	"strings"
+	"time"
 )
 
+/*
+Update: 07/13/2025
+
+Liv has updated their site to use pagination, it's easier now to just call the API directly
+with the current date, and then find out what the next pagination date is from the response
+it also returns the HTML in the response, so I threw out Colly and just use
+goquery to query the HTML
+----
+*/
 func (app *application) fetchLivEdmEvents(w http.ResponseWriter, r *http.Request) {
 	edmEvents := scrapeLivForEdmEvents()
 	err := app.writeJSON(w, http.StatusOK, edmEvents, nil)
@@ -20,12 +31,43 @@ func (app *application) fetchLivEdmEvents(w http.ResponseWriter, r *http.Request
 
 func scrapeLivForEdmEvents() []EdmEvent {
 	edmEvents := []EdmEvent{}
-	scrapeurl := "https://www.livnightclub.com/las-vegas/events/?venue=livlasvegas"
-	c := colly.NewCollector()
-	c.Wait()
+	currentDate := time.Now().Format("2006-01-02")
 
-	c.OnHTML("div.uv-carousel-lat ", func(h *colly.HTMLElement) {
-		selection := h.DOM
+	for {
+		url := fmt.Sprintf("https://www.livnightclub.com/wp-admin/admin-ajax.php?action=uvpx&uvaction=uwspx_loadevents&date=%s&venue=livlasvegas", currentDate)
+
+		resp, err := http.Get(url)
+		if err != nil {
+			break
+		}
+
+		// JSON unmarshal
+		var livEdmEventsResponse LivEdmEventsResponse
+		json.NewDecoder(resp.Body).Decode(&livEdmEventsResponse)
+		resp.Body.Close()
+
+		// GoQuery on the HTML string
+		events := parseHTMLWithGoQuery(livEdmEventsResponse.Agenda)
+		edmEvents = append(edmEvents, events...)
+
+		// Pagination logic
+		if livEdmEventsResponse.Nextloaddate == "" || livEdmEventsResponse.Nevents < 1 {
+			break
+		}
+		currentDate = livEdmEventsResponse.Nextloaddate
+	}
+	return edmEvents
+}
+
+func parseHTMLWithGoQuery(htmlContent string) []EdmEvent {
+	var edmEvents []EdmEvent
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		return edmEvents
+	}
+
+	doc.Find("div.uv-carousel-lat").Each(func(i int, selection *goquery.Selection) {
 		edmEvent := EdmEvent{}
 		artistName := selection.Find("h3.uv-event-name-title").Text()
 		clubName := selection.Find("div.uwsvenuename").Text()
@@ -52,16 +94,14 @@ func scrapeLivForEdmEvents() []EdmEvent {
 		}
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Printf("Error while scraping: %s\n", e.Error())
-	})
-
-	c.Visit(scrapeurl)
-
-	fmt.Println("Scraping Completed for Liv")
 	return edmEvents
+}
+
+type LivEdmEventsResponse struct {
+	Agenda       string `json:"agenda"`
+	Calendar     string `json:"calendar"`
+	List         string `json:"list"`
+	Todate       string `json:"todate"`
+	Nevents      int    `json:"nevents"`
+	Nextloaddate string `json:"nextloaddate"`
 }
